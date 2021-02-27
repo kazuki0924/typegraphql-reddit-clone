d3ac8012e8938dbdaf9013a7836b6e5e7e5388b6
Kazuki Matsuzaki <kazuki0924@gmail.com>
2021-02-27 16:13:08 +0900

    refactor: raw sql

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,69 +1,118 @@
 import {
-	Arg,
-	Ctx,
-	Field,
-	FieldResolver,
-	InputType,
-	Int,
-	Mutation,
-	Query,
 	Resolver,
-	Root,
+	Query,
+	Arg,
+	Mutation,
+	InputType,
+	Field,
+	Ctx,
 	UseMiddleware,
+	Int,
+	FieldResolver,
+	Root,
+	ObjectType,
 } from 'type-graphql';
-import { getConnection } from 'typeorm';
 import { Post } from '../entities/Post';
-import { isAuth } from '../middleware/isAuth';
 import { MyContext } from '../types';
+import { isAuth } from '../middleware/isAuth';
+import { getConnection } from 'typeorm';
 
 @InputType()
 class PostInput {
 	@Field()
 	title: string;
-
 	@Field()
 	text: string;
+}
+
+@ObjectType()
+class PaginatedPosts {
+	@Field(() => [Post])
+	posts: Post[];
+	@Field()
+	hasMore: boolean;
 }
 
 @Resolver(Post)
 export class PostResolver {
 	@FieldResolver(() => String)
-	textSnippet(@Root() root: Post) {
-		return root.text.slice(0, 50);
+	textSnippet(@Root() post: Post) {
+		return post.text.slice(0, 50);
 	}
 
-	@Query(() => [Post])
-	posts(
+	@Query(() => PaginatedPosts)
+	async posts(
 		@Arg('limit', () => Int) limit: number,
 		@Arg('cursor', () => String, { nullable: true }) cursor: string | null
-	): Promise<Post[]> {
-		const qb = getConnection()
-			.getRepository(Post)
-			.createQueryBuilder('post')
-			.orderBy('"createdAt"', 'DESC')
-			.take(Math.min(50, limit));
+	): Promise<PaginatedPosts> {
+		// 20 -> 21
+		const realLimit = Math.min(50, limit);
+		const realLimitPlusOne = realLimit + 1;
+
+		const replacements: Array<string | number | Date> = [realLimitPlusOne];
 
 		if (cursor) {
-			qb.where('"createdAt" < :cursor', {
-				cursor: new Date(parseInt(cursor)),
-			});
+			replacements.push(new Date(parseInt(cursor)));
 		}
 
-		return qb.getMany();
+		const posts = await getConnection().query(
+			/*sql*/ `
+			SELECT
+			_post.*,
+			JSON_BUILD_OBJECT(
+				'id', _user.id,
+				'username', _user.username,
+				'email', _user.email,
+				'createdAt', _user."createdAt",
+				'updatedAt', _user."updatedAt"
+			) AS creator
+			FROM public.post AS _post
+			INNER JOIN public.user AS _user ON _user.id = _post."creatorId"
+			${cursor ? /*sql*/ `WHERE _post."createdAt" < $2` : ''}
+			ORDER BY _post."createdAt" DESC
+			LIMIT $1
+		`,
+			replacements
+		);
+
+		console.log(posts);
+
+		// const qb = getConnection()
+		// 	.getRepository(Post)
+		// 	.createQueryBuilder('p')
+		// 	.innerJoinAndSelect('p.creator', 'u', 'u.id = p."creatorId"')
+		// 	.orderBy('p."createdAt"', 'DESC')
+		// 	.take(realLimitPlusOne);
+
+		// if (cursor) {
+		// 	qb.where('p."createdAt" < :cursor', {
+		// 		cursor: new Date(parseInt(cursor)),
+		// 	});
+		// }
+
+		// const posts = await qb.getMany();
+
+		return {
+			posts: posts.slice(0, realLimit),
+			hasMore: posts.length === realLimitPlusOne,
+		};
 	}
 
 	@Query(() => Post, { nullable: true })
-	post(@Arg('id', () => Int) id: number): Promise<Post | undefined> {
+	post(@Arg('id') id: number): Promise<Post | undefined> {
 		return Post.findOne(id);
 	}
 
-	@Mutation(() => Post, { nullable: true })
+	@Mutation(() => Post)
 	@UseMiddleware(isAuth)
-	createPost(
+	async createPost(
 		@Arg('input') input: PostInput,
 		@Ctx() { req }: MyContext
 	): Promise<Post> {
-		return Post.create({ ...input, creatorId: req.session.userId }).save();
+		return Post.create({
+			...input,
+			creatorId: req.session.userId,
+		}).save();
 	}
 
 	@Mutation(() => Post, { nullable: true })
@@ -71,30 +120,19 @@ export class PostResolver {
 		@Arg('id') id: number,
 		@Arg('title', () => String, { nullable: true }) title: string
 	): Promise<Post | null> {
-		// const post = await em.findOne(Post, { id });
 		const post = await Post.findOne(id);
 		if (!post) {
 			return null;
 		}
 		if (typeof title !== 'undefined') {
-			// post.title = title;
-			// await em.persistAndFlush(post);
 			await Post.update({ id }, { title });
 		}
 		return post;
 	}
 
 	@Mutation(() => Boolean)
-	async deletePost(
-		@Arg('id') id: number
-		// @Ctx() { em }: MyContext
-	): Promise<boolean> {
-		try {
-			// await em.nativeDelete(Post, { id });
-			await Post.delete(id);
-		} catch (err) {
-			return false;
-		}
+	async deletePost(@Arg('id') id: number): Promise<boolean> {
+		await Post.delete(id);
 		return true;
 	}
 }
